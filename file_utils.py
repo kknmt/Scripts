df2850f97ef3eda1e278ee849541be33d74cc2e0
kknmt <105650074+kknmt@users.noreply.github.com>
2023-11-22 00:07:34 +0900

    Create file_utils.py

---
+++ file_utils.py
@@ -0,0 +1,15 @@
+import os
+
+def is_version_downloaded(software_name, version, downloaded_versions_set):
+    combined_version = f"{software_name}-{version}"
+    return combined_version in downloaded_versions_set
+
+def mark_version_as_downloaded(software_name, version, downloaded_versions_set, downloaded_versions_file):
+    combined_version = f"{software_name}-{version}"
+    downloaded_versions_set.add(combined_version)
+    with open(downloaded_versions_file, "a") as file:
+        file.write(f"{combined_version}\n")
+
+def save_file(response, file_path):
+    with open(file_path, "wb") as file:
+        file.write(response.content)
