c32b7b03e2c7c800e1f0a4f93addde51f51c4f94
kknmt <105650074+kknmt@users.noreply.github.com>
2023-11-22 00:42:07 +0900

    Update file_utils.py

--- file_utils.py
+++ file_utils.py
@@ -11,5 +11,7 @@ def mark_version_as_downloaded(software_name, version, downloaded_versions_set, 
         file.write(f"{combined_version}\n")
 
 def save_file(response, file_path):
+    # ファイル保存先ディレクトリが存在しない場合は作成
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, "wb") as file:
         file.write(response.content)
