6da577d06eea7208026533b18985ec440e82f666
kknmt <m20109@g.metro-cit.ac.jp>
2023-11-23 14:01:55 +0000

    deleted:    __pycache__/database_utils.cpython-310.pyc 	deleted:    __pycache__/file_utils.cpython-310.pyc 	modified:   database_utils.py 	modified:   httpd_archive_crawler.py

--- database_utils.py
+++ database_utils.py
@@ -16,30 +16,21 @@ def create_connection():
         database=mysql_database
     )
 
-def create_table(connection, table_name):
-    """Create a table in the MySQL database if it doesn't exist."""
-    cursor = connection.cursor()
-    cursor.execute(f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            version VARCHAR(255) NOT NULL,
-            url TEXT NOT NULL,
-            file_path TEXT NOT NULL,
-            last_modified DATE NOT NULL
-        );
-    """)
-    connection.commit()
-
-def insert_data(db_connection, table_name, data_dict):
+def insert_data(db_connection, table_name, data):
+    cursor = None
+    sql = None
     try:
         cursor = db_connection.cursor()
-        placeholders = ', '.join(['%s'] * len(data_dict))
-        columns = ', '.join(data_dict.keys())
+        placeholders = ', '.join(['%s'] * len(data))
+        columns = 'version, last_modified, file_path'
         sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
-        cursor.execute(sql, tuple(data_dict.values()))
+        print("SQL:", sql)
+        print("Data:", data)
+        cursor.execute(sql, data)
         db_connection.commit()
         return cursor.lastrowid
     except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
-        cursor.close()
+        if cursor:
+            cursor.close()
--- httpd_archive_crawler.py
+++ httpd_archive_crawler.py
@@ -4,7 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from file_utils import is_version_downloaded, mark_version_as_downloaded, save_file
-from database_utils import create_connection, create_table, insert_data
+from database_utils import create_connection, insert_data
 
 # ダウンロード済みのバージョン情報ファイル
 downloaded_versions_file = "/var/repository/downloaded_versions.txt"
@@ -18,22 +18,24 @@ download_dir = "/var/repository/apache/"
 
 # データベースの設定
 db_connection = create_connection()
-table_name = "apache"
-create_table(db_connection, table_name)
+table_name = "apache_2"
 
 # HTMLを取得
 response = requests.get(base_url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# 正規表現: beta版を除外
-beta_regex = re.compile(r".*beta.*", re.IGNORECASE)
-
 # aタグからバージョンとLast Modifiedを取得
 for link in soup.find_all("a"):
     href = link.get("href")
 
-    # ファイル名が.tar.gzで終わり、beta版を除外
-    if href and href.endswith(".tar.gz") and not beta_regex.match(href):
+    # ファイル名が.tar.gzで終わり、beta版、alpha版、deps版を除外
+    if (
+        href
+        and href.endswith(".tar.gz")
+        and "-alpha" not in href
+        and "-deps" not in href
+        and "-bata" not in href
+    ):
         # ファイル名からバージョン情報を正規表現で抽出
         version_match = re.match(r'httpd-(\d+\.\d+\.\d+).*', href)
         if version_match:
@@ -48,53 +50,29 @@ for link in soup.find_all("a"):
                     # 数字とハイフン、コロン以外の文字を削除
                     last_modified = re.sub(r"[^0-9:-]", "", next_sibling).strip()
 
-                    # デバッグ出力
-                    print(f"DEBUG: Last Modified Before: {last_modified}")
-
                     # 日付部分だけ取得
                     date_part = re.search(r"\d{4}-\d{2}-\d{2}", last_modified)
                     if date_part:
                         date_part = date_part.group()
                     else:
-                        print(f"Error: Could not extract date from {last_modified}")
                         continue
-
-                    # デバッグ出力
-                    print(f"DEBUG: Date Part: {date_part}")
 
                     # 日付部分だけ取得
                     last_modified_date = datetime.strptime(date_part, "%Y-%m-%d").date()
-
-                    # デバッグ出力
-                    print(f"DEBUG: Last Modified After: {last_modified_date}")
-
-                    print(f"Downloading Version: {version}, File URL: {base_url + href}, Last Modified: {last_modified_date}")
 
                     # ファイルをダウンロードし、保存
                     response = requests.get(base_url + href)
                     file_path = os.path.join(download_dir, href)
                     save_file(response, file_path)
 
-                    data_dict = {
-                        'version': version,
-                        'url': base_url + href,
-                        'file_path': file_path,
-                        'last_modified': last_modified_date
-                    }
-                    
-
                     # データベースに格納
                     if db_connection:
-                        data = (None, version, base_url + href, file_path, last_modified_date)
-                        insert_data(db_connection, table_name, data_dict)
+                        data = (version, last_modified_date, file_path)  
+                        insert_data(db_connection, table_name, data)
+
 
                     # ダウンロード済みのバージョン情報を追加
                     mark_version_as_downloaded("httpd", version, downloaded_versions, downloaded_versions_file)
-                    print(f"Version {version} processed successfully!")
-                else:
-                    print("Error: Could not find Last Modified information.")
-            else:
-                print(f"Version {version} is already downloaded.")
 
 # データベース接続を閉じる
 if db_connection:
